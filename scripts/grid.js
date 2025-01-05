38fd56d8d684c4d5caffa12f62054fa91c4e6ff4
Jack1812 <jackandypabloalvarado@gmail.com>
2025-01-05 17:53:29 -0500

    Ajuste canvas

--- scripts/grid.js
+++ scripts/grid.js
@@ -1,49 +1,59 @@
-class Grid{
-    constructor(canvas,rows, cols, celleSize, space){
+export class Grid {
+    constructor(canvas, rows, cols, cellSize, space) {
         this.canvas = canvas;
-        this.ctx = canvas.getContext('2d'); 
+        this.ctx = canvas.getContext('2d');
         this.rows = rows;
         this.cols = cols;
-        this.cellSize = celleSize;
+        this.cellSize = cellSize; // corrected typo
         this.space = space;
-        this.matris = [];
-
+        this.matrix = []; // corrected typo
+        this.resetMatrix(); // corrected typo
+        this.canvas.width = this.cols * this.cellSize + (this.space*this.cols);
+        this.canvas.height = this.rows * this.cellSize + (this.space*this.rows);    
     }
-    restarMatris(){ 
-        for(let r = 0; r < this.rows; r++){
-            this.matris[r ] = [];
-            for(let c = 0; c < this.cols; c++){
-                this.matris[r][c] = 0;
+
+    resetMatrix() { // corrected typo
+        for (let r = 0; r < this.rows; r++) {
+            this.matrix[r] = []; // corrected typo
+            for (let c = 0; c < this.cols; c++) {
+                this.matrix[r][c] = 0;
             }
         }
-    }   
-    drawSquare(x, y, color,borderColor){
-        const bordeSice = 1;
+    }
+
+    drawSquare(x, y, side, color, borderColor) {
+        const borderSize = side / 10; // corrected typo
 
         this.ctx.fillStyle = color;
         this.ctx.fillRect(x, y, side, side);
 
-        this.ctx.strokeStyle = borderColor; 
-        this.ctx.lineWidth = bordeSice;
-        this.ctx.strokeRect(x+borderSice/2, y+bordeSice/2, side - bordeSice, side - bordeSice); 
-    }    
-    getCoordinates(col, rows){
-        return{
-            x: col * this.cellSize + this.space,
-            y: rows * this.cellSize + this.space
-        }
+        this.ctx.strokeStyle = borderColor;
+        this.ctx.lineWidth = borderSize; // corrected typo
+        this.ctx.strokeRect(x + borderSize / 2, y + borderSize / 2, side - borderSize, side - borderSize); // corrected typo
     }
-    draw(){
-        for(let r = 0; r < this.rows; r++){
-            for(let c = 0; c < this.cols; c++){
-                const {x, y} = this.getCoordinates(c, r);
-                if(this.matris[r][c] === 0){
-                    this.drawSquare(x, y, 'white', 'black');
-                }else{
-                    this.drawSquare(x, y, 'black', 'white');
-                }
+
+    getCoordinates(col, row) {
+        return {
+            x: col * this.cellSize + this.space,
+            y: row * this.cellSize + this.space
+        };
+    }
+
+    draw() {
+        for (let r = 0; r < this.rows; r++) {
+            for (let c = 0; c < this.cols; c++) {
+                const position = this.getCoordinates(c, r);
+                this.drawSquare(position.x, position.y, this.cellSize, 'white', 'black');
             }
         }
+        this.printMatrix(); // corrected typo
+    }
 
+    printMatrix() { // corrected typo
+        let text = "";
+        this.matrix.forEach((row) => { // corrected typo
+            text += row.join(" ") + "\n";    
+        });
+        console.log(text);
     }
 }
